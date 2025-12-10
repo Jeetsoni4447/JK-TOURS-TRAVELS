0c6a84dd708e1badf0af841e1176747fd30b92d9
Jeetsoni4447 <codecrafter4447@gmail.com>
2025-12-10 21:22:35 +0530

    finally full wesbite is update

--- src/Components/Review/Review.jsx
+++ src/Components/Review/Review.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState, useRef } from 'react';
 import './Review.css';
+
 function Review() {
 
   const ref = useRef(null);
@@ -9,11 +10,11 @@ function Review() {
     const observer = new IntersectionObserver(
       ([entry]) => {
         if (entry.isIntersecting) {
-          setAnimate(false);  // reset
-          setTimeout(() => setAnimate(true), 10); // restart animation
+          setAnimate(false);
+          setTimeout(() => setAnimate(true), 10);
         }
       },
-      { threshold: 0.5 } // 50% visible
+      { threshold: 0.5 }
     );
 
     if (ref.current) observer.observe(ref.current);
@@ -21,36 +22,18 @@ function Review() {
     return () => observer.disconnect();
   }, []);
 
-  useEffect(() => {
-    const hiddenBoxes = document.querySelectorAll('.transition_hide');
-
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add('transition_show');
-        } else {
-          entry.target.classList.remove('transition_show');
-        }
-      });
-    });
-
-    hiddenBoxes.forEach((box) => observer.observe(box));
-
-    return () => {
-      hiddenBoxes.forEach((box) => observer.unobserve(box));
-    };
-  }, []);
-
   return (
     <>
       <div className="review transition_hide">
         <div className={`google ${animate ? "animate" : ""}`} ref={ref}>
-          Jk Tours & Travels Google Review
+          JK Tours & Travels Google Reviews
         </div>
-        <iframe className="review_comments" src='https://widgets.sociablekit.com/google-reviews/iframe/25628054' frameBorder='0' width='100%' height='1000'></iframe>
+
+        <iframe src='https://widgets.sociablekit.com/google-reviews/iframe/25631965' frameBorder='0' width='100%' height='1000'></iframe>
+
       </div>
     </>
-  )
+  );
 }
 
 export default Review;
